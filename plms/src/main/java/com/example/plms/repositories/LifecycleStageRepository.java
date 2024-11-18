e7bf8b58c451433725a966b7dc914eda929a375b
Jeyakumar N K <121599159+Jeyakumar30@users.noreply.github.com>
2024-11-19 01:30:50 +0530

    Delete plms directory

--- plms/src/main/java/com/example/plms/repositories/LifecycleStageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.plms.repositories;
-
-import com.example.plms.models.LifecycleStage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LifecycleStageRepository extends JpaRepository<LifecycleStage, Long> {
-}
